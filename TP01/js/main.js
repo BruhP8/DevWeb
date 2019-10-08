14f8b519bd4f3610ff1dc2619c4ed26a2aeeabf7
BruhP8 <luc.biohazard@gmail.com>
2019-10-08 19:20:29 +0200

    commit du : j'ai oublié de commit donc je fais tout comme un porc

--- TP01/js/main.js
+++ TP01/js/main.js
@@ -3,18 +3,61 @@ window.addEventListener("load", event => {
 	main();
 });
 
+let carreArray = [];
+let data = ["room 1", "room 2", "room 3", "room 4", "room 5", "room 6", "room 7",
+					  "room 8", "room 9", "room 10", "room 11"];
+
 const main = event => {
 	
 	console.log("MAIN");
-	
-	let component = new MyComponent("dummy");
-	
+	data.forEach(temp => {
+		carreArray.push(CreateCarre(temp));
+	});
+	carreArray.forEach(tempd => {
+		PlaceCarre(tempd);
+	});
 }
 
-class MyComponent {
-	
-	constructor(name) {
-		console.log("my name is", name);
-	}
-	
+const PlaceCarre = (carre) => {
+	carre.style.position = "absolute";
+	let Coord = CoordFinder(carreArray.indexOf(carre));
+	carre.style.left = Coord[0] + "px";
+	console.log(Coord[0]);
+	carre.style.top = Coord[1] - 20+ "px";
+	console.log(Coord[1]);
+
+}
+
+const CreateCarre = (iteration) =>{
+	let carre = document.createElement("div");
+	carre.style.backgroundColor = "blue";
+	carre.innerHTML = iteration;
+	carre.addEventListener("click", event => {MoveDown()}); 
+	document.body.appendChild(carre);
+	return carre;
+}
+
+const CoordFinder = (index) =>{
+	console.log(index)
+	let x = 0;
+	let y = (window.innerHeight/(6)) * (index+1);
+	x = -Math.pow(y - window.innerHeight/2, 2)/1000 + 100;
+	let size = 40 * x/100
+	carreArray[index].style.width = (size * 2) + "px"
+	carreArray[index].style.height = (size * 2) + "px"
+	return [x, y, index];
+}
+
+const MoveDown = () =>{
+	console.log("\n ---------------------------------- \n");
+	data.push(data[0]);
+	data.shift();
+	document.body.innerHTML = "";
+	carreArray.length = 0;
+	data.forEach(temp => {
+		carreArray.push(CreateCarre(temp));
+	});
+	carreArray.forEach(tempd => {
+		PlaceCarre(tempd);
+	});
 }
