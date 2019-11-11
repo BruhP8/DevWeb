61b83dd2e88be184aca9a2e198792bf96dfc6e01
BruhP8 <luc.biohazard@gmail.com>
2019-11-11 17:06:59 +0100

    Comprehension du problème : le server n'est pas a traiter comme avant dans le navigateur mais avec node en terminal

--- TP05/server.js
+++ TP05/server.js
@@ -1,6 +1,6 @@
 var http = require('http');
 
-const server = http.CreateServer((req, res) =>{
+const server = http.createServer((req, res) =>{
 	console.log("hey, you recieved a request bitch")
 })
 
